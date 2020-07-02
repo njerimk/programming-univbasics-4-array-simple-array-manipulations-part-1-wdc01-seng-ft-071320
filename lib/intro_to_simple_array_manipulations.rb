e6a2a872e23dfa05315ca461e551016fdec253f9
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-02 18:15:05 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,32 @@
+def using_push (array, string)
+  array=["happy","sad","serious"]
+  string="violet"
+  p array.push(string)
+end
+
+def using_unshift(array,string)
+  array=["hello","thank-you", "good-bye","good evening"]
+  string="Staten Island"
+  p array.unshift(string)
+end 
+
+def using_pop(array)
+  @continents=["North America","South America", "Africa", "Asia", "Australia", "Europe","Antarctica"]
+  @continents.pop
+end
+
+def pop_with_args(array)
+  @dog_breeds=["Chihuahua","Shiba Inu"]
+end
+
+def using_shift(array)
+  @im_so_over_this_city=using_shift(@my_favorite_cities)
+end
+
+def shift_with_args(array)
+  @im_so_over_this_city = ["Tokyo", "Los Angeles", "New York City"]
+  @im_so_over_this_city.shift
+  im_so_over_this
+  @brands_removed=["Blue Bell Creameries", "Ben & Jerry's"]
+    @brands_removed.shift
+end
